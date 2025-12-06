16455de639cc7cbb8e1516021bc9d9b78b2acd3f
Harry Ji <jiharry@hotmail.com>
2025-12-06 12:06:29 -0800

    black

--- 2025/12/2025-12-06/vite.config.ts
+++ 2025/12/2025-12-06/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: './', // Output to current directory
+    emptyOutDir: false, // IMPORTANT: Don't delete source files!
+    chunkSizeWarningLimit: 2000,
+  }
 })
